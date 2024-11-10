7192048992a4c1a732c5a3f28beda25fc46f0cf5
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-11-10 12:39:29 -0600

    Create counting_dna_nucleotides.py
    My solution for the same problem in Rosalind

---
+++ solutions/counting_dna_nucleotides.py
@@ -0,0 +1,17 @@
+def count(path: str) -> tuple((str, str, str, str)): # type: ignore
+    f = open(path, "r")
+    dna = f.read()
+    f.close()
+    A, C, G, T = 0, 0, 0, 0
+    for base in dna:
+        match base:
+            case "A": A += 1
+            case "C": C += 1
+            case "G": G += 1
+            case "T": T += 1
+    return (A, C, G, T)
+
+
+if __name__ == "__main__":
+    path = ""   # insert path
+    print(count(path))
