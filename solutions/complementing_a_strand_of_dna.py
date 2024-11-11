b289244cde36abd482e30eb40e6f6004b83c2060
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2024-11-10 18:10:59 -0600

    Create complementing_a_strand_of_dna.py

---
+++ solutions/complementing_a_strand_of_dna.py
@@ -0,0 +1,17 @@
+def reverse_complement(path: str) -> str:
+    f = open(path, "r")
+    template = f.read()
+    f.close()
+    rc = ""
+    for base in template:
+        match base:
+            case "A": rc += "T"
+            case "T": rc += "A"
+            case "C": rc += "G"
+            case "G": rc += "C"
+    return rc[::-1]
+
+
+if __name__ == "__main__":
+    path = ""   # insert path
+    print(reverse_complement(path))
