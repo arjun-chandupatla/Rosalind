d85b0d62405e4638878544750cd58b9bb3703f76
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-04-23 17:06:38 -0500

    Update parse_fasta.py

--- solutions/parse_fasta.py
+++ solutions/parse_fasta.py
@@ -1,1 +1,22 @@
 # FASTA file format parser
+
+def parse_fasta(path):
+    f = open(path, "r")
+    id = None
+    d = dict()
+    while True:
+        line = f.readline()
+        line = line.rstrip()
+        if not line:
+            f.close()
+            return d
+        if line.startswith(">"):
+            id = line
+            d[id] = """"""
+        else:
+            d[id] += line
+
+
+if __name__ == "__main__":
+    path = ""   # insert path
+    print(parse_fasta(path))
