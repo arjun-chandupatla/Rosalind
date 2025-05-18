cfb0bca3f69b2f80712b52805ff31485540a9150
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-18 10:28:18 -0500

    Update hamming_distance.py

--- solutions/hamming_distance.py
+++ solutions/hamming_distance.py
@@ -1,1 +1,9 @@
 # return the "difference" in two strings
+# difference is measured by number of mismatches
+
+def hamming_distance(p: str, q: str) -> int:
+    dist = 0
+    for i in range(len(p)):
+        if p[i] != q[i]:
+            dist += 1
+    return dist
