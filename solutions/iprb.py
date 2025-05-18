9aa4175ca401efa6eddf0462342d56a7058368f8
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-18 11:12:22 -0500

    Update iprb.py

--- solutions/iprb.py
+++ solutions/iprb.py
@@ -1,2 +1,12 @@
 # Mendel's First Law
 # probabilities mostly
+
+
+def dominant(k: int, m: int, n: int) -> float:
+    Pr1 = lambda x: x/float(k+m+n)
+    Pr2 = lambda x: x/float(k+m+n-1)
+    sum = 0
+    sum += Pr1(k)
+    sum += (Pr1(m) * Pr2(k)) + (0.75)*(Pr1(m) * Pr2(m-1)) + (0.5)*(Pr1(m) * Pr2(n))
+    sum += (Pr1(n)*Pr2(m)) + 0.5*(Pr1(n) * Pr2(m))
+    return sum
