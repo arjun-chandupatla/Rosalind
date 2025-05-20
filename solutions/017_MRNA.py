c2b413c04c265cf65e4f7f5db0d89be356a8b052
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-20 08:44:48 -0500

    Create 017_MRNA.py

---
+++ solutions/017_MRNA.py
@@ -0,0 +1,20 @@
+num_pep = {"F":2, "L":6, "S":6, "Y":2, "C":2, "STOP":3, 
+           "W":1, "P":4, "H":2, "Q":2, "R":6, "I":3, 
+           "M":1, "T":4, "N":2, "K":2, "V":4, "A":4, 
+           "D":2, "G":4, "E":2}
+
+
+def numMrna(pep: str) -> int:
+    global num_pep
+    num = 1
+    for aa in pep:
+        if aa.isspace():
+            pass
+        else:
+            num *= num_pep[aa]
+            if num > 1000000:
+                num %= 1000000
+                if num == 0:
+                    return 0
+    num *= 3        # stop codon
+    return num % 1000000
