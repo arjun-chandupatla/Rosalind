d6224e3872edf3af62a65e57b092cfb69a3cdad5
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-20 08:47:06 -0500

    Update 017_MRNA.py

--- solutions/017_MRNA.py
+++ solutions/017_MRNA.py
@@ -1,8 +1,12 @@
-num_pep = {"F":2, "L":6, "S":6, "Y":2, "C":2, "STOP":3, 
-           "W":1, "P":4, "H":2, "Q":2, "R":6, "I":3, 
-           "M":1, "T":4, "N":2, "K":2, "V":4, "A":4, 
-           "D":2, "G":4, "E":2}
+# Given a peptide, return the number of 
+# possible mRNA strands that could encode
+# that peptide modulo 1,000,000
 
+# Number of codons per peptide
+num_pep = {"F":2, "L":6, "S":6, "Y":2, "C":2, "W":1, 
+           "P":4, "H":2, "Q":2, "R":6, "I":3, "M":1, 
+           "T":4, "N":2, "K":2, "V":4, "A":4, "D":2, 
+           "G":4, "E":2}
 
 def numMrna(pep: str) -> int:
     global num_pep
