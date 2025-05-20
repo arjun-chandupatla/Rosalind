174287d2a619c56bd63f00d5e0f69eef3211141b
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-19 19:44:32 -0500

    Update parse_fasta.py

--- solutions/005_GC/parse_fasta.py
+++ solutions/005_GC/parse_fasta.py
@@ -1,1 +1,18 @@
+# FASTA file format parser
+# Used to parse the input format for 005_GC on Rosalind
 
+def parseFasta(path):
+    f = open(path, "r")
+    id = None
+    d = dict()
+    while True:
+        line = f.readline()
+        line = line.rstrip()
+        if not line:
+            f.close()
+            return d
+        if line.startswith(">"):
+            id = line
+            d[id] = """"""
+        else:
+            d[id] += line
