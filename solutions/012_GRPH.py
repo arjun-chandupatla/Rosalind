935173a1b53fa45846bb0b1e69aebd0e6e27281b
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-22 18:31:35 -0500

    Documentation

--- solutions/012_GRPH.py
+++ solutions/012_GRPH.py
@@ -1,3 +1,6 @@
+# Create an overlap graph O_3 given a series of DNA strands in FASTA format
+
+# Creates the overlap graph as a hashmap with the initial strand as a key and the following strand as a value
 def overlapGraph(inp: dict[str, str], k: int) -> dict[str, list[str]]:
     pattern_set = dict()
 
@@ -15,14 +18,18 @@ def overlapGraph(inp: dict[str, str], k: int) -> dict[str, list[str]]:
     return pattern_set
 
 
+# Returns the first k elements of a string
 def prefix(kmer: str, k: int) -> str:
     return kmer[:k]
 
 
+# Returns the last k elements of a string
 def suffix(kmer: str, k: int) -> str:
     return kmer[-k:]
 
 
+# Converts the FASTA form input, in a text file, to a hashmap
+# overlapGraph reads the hashmap and creates the graph based on that
 def parseInput(file):
     d = dict()
     temp = ""
@@ -38,6 +45,7 @@ def parseInput(file):
     return d
 
 
+# Creates a Rosalind-friendly string output
 def formatOutput(ans: dict[str, list[str]]) -> str:
     s = ""
     for k in ans.keys():
