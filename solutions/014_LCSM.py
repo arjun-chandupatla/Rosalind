b27565e491873a87df9c9a6952078a4f1cc6dc65
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-23 16:50:00 -0500

    Update 014_LCSM.py

--- solutions/014_LCSM.py
+++ solutions/014_LCSM.py
@@ -1,3 +1,20 @@
 # Find the longest common substring of a set of DNA strings
 # This substring is most likely a motif, and its function can be analyzed further
 
+def parseInput(file) -> list[str]:
+    dna = list()
+    temp = ""
+    for line_s in file:
+        line = line_s.strip()
+        if line.isspace():
+            continue
+        elif line.startswith(">"):
+            if len(temp) == 0:
+                pass
+            else:
+                dna.append(temp)
+                temp = ""
+        else:
+            temp += line.strip()
+    dna.append(temp)
+    return dna
