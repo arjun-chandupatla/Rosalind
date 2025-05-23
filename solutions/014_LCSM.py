4393bffd4e0413ebc5a595069da70378cf22e949
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-23 16:56:40 -0500

    Documentation

--- solutions/014_LCSM.py
+++ solutions/014_LCSM.py
@@ -1,6 +1,7 @@
 # Find the longest common substring of a set of DNA strings
 # This substring is most likely a motif, and its function can be analyzed further
 
+# Turns input (text file in FASTA format) into a set of strings
 def parseInput(file) -> list[str]:
     dna = list()
     temp = ""
@@ -20,26 +21,30 @@ def parseInput(file) -> list[str]:
     return dna
 
 
+# Expands a sequence by adding a nucleotide
 def expand(dna: str):
     for base in "ATCG":
         yield dna + base
 
 
+# Iteratively create and test substrings
+# Returns ALL longest substrings, if there are multiple with the same length
 def findMotifs(dna_arr: list[str]) -> list[str]:
     motifs = []
     new_motifs = [""]
-    while len(new_motifs) != 0:
+    while len(new_motifs) != 0: # when len(new_motifs) is 0, then the longest substring is found
         motifs = new_motifs
         new_motifs = []
         for m in motifs:		# regenerate new_motifs
             new_motifs += list(expand(m))
-        for d in dna_arr:
+        for d in dna_arr:       # prune new_motifs
             for i in range(len(new_motifs) - 1, -1, -1):
                 if new_motifs[i] not in d:
                     del new_motifs[i]
     return motifs
 
 
+# Rosalind only asks for one substring, so this only returns one
 def formatOutput(motifs: list[str]) -> str:
     s = motifs[0]
     return s
