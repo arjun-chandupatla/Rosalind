2486d4814e8c027df619adee7d15b412baa6aab8
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-22 21:54:12 -0500

    Update 010_CONS.py

--- solutions/010_CONS.py
+++ solutions/010_CONS.py
@@ -1,2 +1,24 @@
 # Create a consensus strand and a profile matrix
 # for a set of DNA strands provided
+
+# Create the profile matrix
+def count(motifs):
+    tMotifs = []
+    for column in transpose(motifs):
+        tMotifs.append(list(column))
+    k = len(tMotifs[0])
+    A = [0] * k
+    C = [0] * k
+    G = [0] * k
+    T = [0] * k
+    for i in range(k):
+        for r in tMotifs:
+            if r[i] == "A":
+                A[i] += 1
+            elif r[i] == "C":
+                C[i] += 1
+            elif r[i] == "G":
+                G[i] += 1
+            elif r[i] == "T":
+                T[i] += 1
+    return [A, C, G, T]
