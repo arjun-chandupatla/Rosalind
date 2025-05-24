07a6e7ea8b2b0356bceba95e85e5d5c54279ac94
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-24 08:40:22 -0500

    Update 013_IEV.py

--- solutions/013_IEV.py
+++ solutions/013_IEV.py
@@ -1,1 +1,18 @@
-
+def getOffspring(n_arr: list[int]) -> float:
+    offspring_arr = []
+    for i in range(6):
+        k = i+1
+        n = n_arr[i]
+        match k:
+            case 1 | 2 | 3:
+                offspring_arr.append(float(n*2))
+            case 4:
+                offspring_arr.append(float(2*n*0.75))
+            case 5:
+                offspring_arr.append(float(n))
+            case 6:
+                offspring_arr.append(float(0))
+            case _:
+                pass
+    num_dominant = sum(offspring_arr)
+    return num_dominant
