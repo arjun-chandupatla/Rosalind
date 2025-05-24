f3558b70c7b43f6d122cc62d4f4370b129630467
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-24 08:04:08 -0500

    Update 018_ORF.py

--- solutions/018_ORF.py
+++ solutions/018_ORF.py
@@ -1,1 +1,3 @@
-
+# Output all the possible proteins that a strand of DNA could
+# encode for. These start with the start codon and end with the
+# stop codon in both the forward strand and its complement.
