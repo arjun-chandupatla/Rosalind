b6cce3898e63ba6052e8b727e6eee9dbf499d079
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-25 11:06:29 -0500

    Update 015_LIA.py

--- solutions/015_LIA.py
+++ solutions/015_LIA.py
@@ -11,11 +11,6 @@ def getProbOffspring(k: int, N: int) -> float:
     prob = 0.0
     P = 2**k
     for i in range(N, P+1):
-        nCk = ( f(P) / (f(i) * f(P-i)) )
-        one4 = (0.25)**i
-        three4 = (0.75)**(P-i)
-        total = nCk + one4 + three4
-
         prob += ( f(P) / (f(i) * f(P-i)) ) * (0.25)**i * (0.75)**(P-i)
     return prob
 
