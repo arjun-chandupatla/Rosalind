bc24ad0dd93ddbe91e1f780d30af4c1428a7b7c4
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-26 15:21:02 -0500

    Update splc.py

--- solutions/splc.py
+++ solutions/splc.py
@@ -1,1 +1,26 @@
 
+def translate(rna: str) -> str:
+    gen_code = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+    
+    protein = ""
+    for i in range(0, len(rna), 3):
+        codon = rna[i:i+3]
+        if gen_code[codon] == "STOP":
+            return protein
+        protein += gen_code[codon]
+    raise Exception("translation error, stop codon not found")
