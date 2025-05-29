d25c43a6d7c16ecdd864d682395ec794269c1750
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-28 19:35:51 -0500

    Update 028_PROB.py

--- solutions/028_PROB.py
+++ solutions/028_PROB.py
@@ -4,3 +4,15 @@
 # a higher probability
 
 
+def probRandomString(s: str, gc_arr: list[float]):
+    for gc in gc_arr:
+        g = gc/2
+        a = (1-gc)/2
+        prob = 1.0
+        for base in s:
+            match base:
+                case "A" | "T":
+                    prob *= a
+                case "C" | "G":
+                    prob *= g
+        yield math.log10(prob)
