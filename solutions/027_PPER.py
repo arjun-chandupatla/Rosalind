bb496b8ab7475a7d008e0fa66946e2f13ab513c2
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-28 19:27:37 -0500

    Update 027_PPER.py

--- solutions/027_PPER.py
+++ solutions/027_PPER.py
@@ -1,2 +1,13 @@
 # Find the number of partial permutations mod 10^6
 # Computes nPk with factorials
+
+
+def partialPerms(n: int, k: int) -> int:
+    num = 1
+    for i in range(n-k+1, n+1):
+        num *= i
+        if num > 1000000:
+            num %= 1000000
+        elif num == 1000000:
+            return 0
+    return num
