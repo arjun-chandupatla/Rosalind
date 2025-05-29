914e37d9c18cf6eaefa9a1509798e11258c313a3
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-28 19:35:36 -0500

    Update 028_PROB.py

--- solutions/028_PROB.py
+++ solutions/028_PROB.py
@@ -1,5 +1,6 @@
 # Basically finds the probability that a motif will occur through random chance in 
 # a DNA sequence given its GC content
-
+# Takes the log base 10 of each probability, so higher numbers (less negative) mean
+# a higher probability
 
 
