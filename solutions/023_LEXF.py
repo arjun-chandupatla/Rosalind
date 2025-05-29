e552ab0dfcfbeede5383ee95866a9c420e170c07
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-28 19:24:58 -0500

    Update 023_LEXF.py

--- solutions/023_LEXF.py
+++ solutions/023_LEXF.py
@@ -3,10 +3,14 @@
 
 from itertools import product
 
+
+# Computes the list of kmers
+# This list is sorted if alphabet is sorted
 def allKmers(alphabet: list[str], n: int):
     return list(product(alphabet, repeat=n))
 
 
+# Format the list into a string for Rosalind
 def formatOutput(ans) -> str:
     s = ""
     for i in ans:
