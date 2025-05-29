b0749d600e849a186a7d1e978e92ed58e5ac868d
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-28 19:22:38 -0500

    Update 023_LEXF.py

--- solutions/023_LEXF.py
+++ solutions/023_LEXF.py
@@ -1,3 +1,16 @@
 # List all k-mers alphabetically provided an alphabet and a length
 # Computes the Cartesian product of the alphabet set with itertools
 
+from itertools import product
+
+def allKmers(alphabet: list[str], n: int):
+    return list(product(alphabet, repeat=n))
+
+
+def formatOutput(ans) -> str:
+    s = ""
+    for i in ans:
+        for j in i:
+            s += j
+        s += "\n"
+    return s
