4aae9e92f71fbe586d8953d7981bb90bd7ac0904
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-29 20:18:39 -0500

    Update 007_IPRB.py

--- solutions/007_IPRB.py
+++ solutions/007_IPRB.py
@@ -1,11 +1,11 @@
 # Mendel's First Law
-# probabilities mostly
+# Deals with probabilities
 
 
 def dominant(k: int, m: int, n: int) -> float:
     Pr1 = lambda x: x/float(k+m+n)
     Pr2 = lambda x: x/float(k+m+n-1)
-    sum = 0
+    sum = 0        # Following numbers come from tree
     sum += Pr1(k)
     sum += (Pr1(m) * Pr2(k)) + (0.75)*(Pr1(m) * Pr2(m-1)) + (0.5)*(Pr1(m) * Pr2(n))
     sum += (Pr1(n)*Pr2(m)) + 0.5*(Pr1(n) * Pr2(m))
