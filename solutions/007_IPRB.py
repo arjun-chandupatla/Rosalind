60c329d294b9cad3b17bcd46febc5fe1322770ea
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-29 20:18:50 -0500

    Update 007_IPRB.py

--- solutions/007_IPRB.py
+++ solutions/007_IPRB.py
@@ -5,7 +5,7 @@
 def dominant(k: int, m: int, n: int) -> float:
     Pr1 = lambda x: x/float(k+m+n)
     Pr2 = lambda x: x/float(k+m+n-1)
-    sum = 0        # Following numbers come from tree
+    sum = 0        # Following numbers come from drawing a possibility tree
     sum += Pr1(k)
     sum += (Pr1(m) * Pr2(k)) + (0.75)*(Pr1(m) * Pr2(m-1)) + (0.5)*(Pr1(m) * Pr2(n))
     sum += (Pr1(n)*Pr2(m)) + 0.5*(Pr1(n) * Pr2(m))
