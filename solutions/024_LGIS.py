bf4b9b41304b5bf60243a6458ff90c7a10d458cb
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-01 11:06:38 -0500

    Update 024_LGIS.py

--- solutions/024_LGIS.py
+++ solutions/024_LGIS.py
@@ -6,8 +6,9 @@ def subsequence(p: list) -> list[int]:
         for j in range(i+1, len(p)):
             if p[i] < p[j]:
                 l[j] = max(l[j], l[i] + 1)
+    # l now holds the length of LIS at each index
     ssq_arr = []
-    max_len = max(l)
+    max_len = max(l)    # LIS length
     for i in range(len(p) - 1, -1, -1):
         if max_len == l[i]:
             ssq_arr.append(p[i])
