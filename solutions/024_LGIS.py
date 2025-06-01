733ade5128c16c711f37c85ca12bd9111da8e0db
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-01 11:03:38 -0500

    Update 024_LGIS.py

--- solutions/024_LGIS.py
+++ solutions/024_LGIS.py
@@ -1,1 +1,15 @@
 # Find longest increasing subsequence
+
+def subsequence(p: list) -> list[int]:
+    l = [1] * len(p)
+    for i in range(len(p)):
+        for j in range(i+1, len(p)):
+            if p[i] < p[j]:
+                l[j] = max(l[j], l[i] + 1)
+    ssq_arr = []
+    max_len = max(l)
+    for i in range(len(p) - 1, -1, -1):
+        if max_len == l[i]:
+            ssq_arr.append(p[i])
+            max_len -= 1
+    return ssq_arr[::-1]
