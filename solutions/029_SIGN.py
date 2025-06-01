823bdafc79696cbda8d4c9b734ff773d718f859a
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-31 21:40:27 -0500

    Update 029_SIGN.py

--- solutions/029_SIGN.py
+++ solutions/029_SIGN.py
@@ -1,3 +1,21 @@
 # Given a number n <= 6, return all the possible signed permutations of length n
 # A signed permutation is a permutation, but every number is prefaced with either 
 # a positive or a negative sign
+
+
+def getPerms(n: int):
+    alph = list(range(1, n+1))
+    l = len(alph)
+    for i in range(l):
+        term = alph[i]
+        alph.append(-term)
+
+    perms = list(permutations(alph, n))
+    for j in range(len(perms) - 1, -1, -1):
+        item = list(perms[j])
+        temp = list(map(abs, item))
+        if len(temp) != len(set(temp)):
+            del perms[j]
+        
+
+    return perms
