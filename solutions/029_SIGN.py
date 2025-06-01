b8781a24183a98336145663f3302c6847b55899f
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-05-31 21:40:13 -0500

    Update 029_SIGN.py

--- solutions/029_SIGN.py
+++ solutions/029_SIGN.py
@@ -1,1 +1,3 @@
 # Given a number n <= 6, return all the possible signed permutations of length n
+# A signed permutation is a permutation, but every number is prefaced with either 
+# a positive or a negative sign
