a85b3d42a4da3e48009aace621476a1c23ba932b
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-02 17:27:02 -0500

    Update 025_LONG.py

--- solutions/025_LONG.py
+++ solutions/025_LONG.py
@@ -12,3 +12,27 @@ def overlap(s: str, t: str) -> int:
         if s[-i:] == t[:i]:
             o = i
     return o
+
+
+def getOrder(dna: list[str]):
+    order = dict()
+    for i in range(len(dna)):
+        d1 = dna[i]
+        overlap_arr = [0] * len(dna)
+        for j in range(len(dna)):
+            if i == j: continue
+            d2 = dna[j]
+            overlap_arr[j] = overlap(d1, d2)
+        idx = overlap_arr.index(max(overlap_arr))
+        order[d1] = dna[idx]
+    
+    l = []
+    for k in order.keys():
+        if k not in order.values():
+            l = [k]
+            break
+
+    for _ in range(1, len(dna)):
+        l.append(order[l[-1]])
+
+    return l
