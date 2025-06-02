a6daab888b3ceef158d984a22d27ee4f4d92540a
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-02 17:25:59 -0500

    Documentation

--- solutions/025_LONG.py
+++ solutions/025_LONG.py
@@ -3,10 +3,11 @@
 # is the shortest superstring of the DNA set
 
 
+# Finds the length of overlap between two strings
 def overlap(s: str, t: str) -> int:
     if s == t:
         return len(s)
-    o = -1
+    o = -1      # Maximum overlap
     for i in range(1, min(len(s), len(t))):
         if s[-i:] == t[:i]:
             o = i
