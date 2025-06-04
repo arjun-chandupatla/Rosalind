1849d6101ea3906c87f87fc0d45cb86030db9f0b
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-03 19:36:18 -0500

    Update tree.py

--- solutions/tree.py
+++ solutions/tree.py
@@ -2,17 +2,6 @@
 # return the minimum number of edges that can be added to this graph to make a tree
 
 
-def parseInput(file):
-    lines = file.readlines()
-    n = int(lines[0].strip())
-    l = list()
-    temp = []
-    for i in range(1, len(lines)):
-        line = lines[i].strip()
-        l.append([int(j) for j in line.split()])
-    
-    return (n, l)
-
 
 
 def func(n, edge_list: list[list[int]]) -> int:
@@ -59,5 +48,22 @@ def func(n, edge_list: list[list[int]]) -> int:
 
     return num
 
+
+
+
+
+
+
+
+def parseInput(file):
+    lines = file.readlines()
+    n = int(lines[0].strip())
+    l = list()
+    temp = []
+    for i in range(1, len(lines)):
+        line = lines[i].strip()
+        l.append([int(j) for j in line.split()])
+    
+    return (n, l)
 
 
