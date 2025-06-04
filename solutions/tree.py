8da8f3084a3fd30974d9b86a3fded28bb964f09e
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-03 19:34:13 -0500

    Update tree.py

--- solutions/tree.py
+++ solutions/tree.py
@@ -1,1 +1,2 @@
-# minimum number of edges to make a tree
+# Given a positive integer n ≤ 1000 and an adjacency list for a graph with n nodes and no cycles,
+# return the minimum number of edges that can be added to this graph to make a tree
