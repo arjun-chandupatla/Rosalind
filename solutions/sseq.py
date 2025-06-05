6a11845c83e977679cbf90cf1352d28f6dffaf7a
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-04 19:04:18 -0500

    Update sseq.py

--- solutions/sseq.py
+++ solutions/sseq.py
@@ -1,2 +1,12 @@
 # find the indices of a subsequence the subsequence doesn't have to occur contiguously
 # this reflects how motifs/genes aren't always contiguous, as well as the splicing process
+
+
+def findIndices(dna: str, seq: str) -> list[int]:
+    index_arr = [0]
+
+    for base in seq:
+        index_arr.append(dna.index(base, index_arr[-1]) + 1)
+
+        
+    return index_arr[1:]
