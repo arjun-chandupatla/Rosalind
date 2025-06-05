7ba6311a70fc2a39d239f3b7509fb3c2064b46ae
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-04 19:03:59 -0500

    Update sseq.py

--- solutions/sseq.py
+++ solutions/sseq.py
@@ -1,2 +1,2 @@
-# find the indices of a subsequence
-# the subsequence doesn't have to occur contiguously
+# find the indices of a subsequence the subsequence doesn't have to occur contiguously
+# this reflects how motifs/genes aren't always contiguous, as well as the splicing process
