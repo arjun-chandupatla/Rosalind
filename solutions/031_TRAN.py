ab8aaabebd8270e9a0a624234c84c8fcf1ed59bf
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-06 13:59:33 -0500

    Update 031_TRAN.py

--- solutions/031_TRAN.py
+++ solutions/031_TRAN.py
@@ -3,3 +3,17 @@
 # but transitions are purine-purine or pyrimidine-pyrimidine substitutions,
 # while transversions are purine-pyrimidine substitutions
 
+def ratio(s1: str, s2: str) -> float:
+    ts, tv = 0, 0
+    for i in range(len(s1)):
+        b1, b2 = s1[i], s2[i]
+        if b1 == b2:
+            continue
+        match b1, b2:
+            case "A" | "G", "A" | "G":
+                ts += 1
+            case "C" | "T", "C" | "T":
+                ts += 1
+            case _:
+                tv += 1
+    return ts/tv
