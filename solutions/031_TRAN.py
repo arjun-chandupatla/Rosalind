817210df2850982636e35ad3091318a5f59141da
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-06 13:59:19 -0500

    Update 031_TRAN.py

--- solutions/031_TRAN.py
+++ solutions/031_TRAN.py
@@ -1,1 +1,5 @@
 # Find the ratio of transitions to transversions in a string of DNA
+# Both transitions and transversions are types of point mutations,
+# but transitions are purine-purine or pyrimidine-pyrimidine substitutions,
+# while transversions are purine-pyrimidine substitutions
+
