522d4ac1d8cd4e72bf7262eb4a0de06223a265d8
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-07 15:40:16 -0500

    Update 043_rstr.py

--- solutions/043_rstr.py
+++ solutions/043_rstr.py
@@ -1,3 +1,16 @@
 # random string matching
 
 import numpy as np
+def getProb(n: int, gc: float, motif: str):
+    AT, GC = 0, 0     
+    for base in motif:
+        if base == "A":
+            AT += 1
+        elif base == "T":
+            AT += 1
+        elif base == "C":
+            GC += 1
+        elif base == "G":
+            GC += 1
+    p = ((0.5 * gc)**GC) * ((0.5 * (1 - gc))**AT)
+    return 1 - (1-p)**n
