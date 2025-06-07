948a10eb6108fcc5fa0d567c5dce402026977948
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-07 15:41:24 -0500

    Update 043_rstr.py

--- solutions/043_rstr.py
+++ solutions/043_rstr.py
@@ -3,9 +3,10 @@
 def getProb(n: int, gc: float, motif: str):
     AT, GC = 0, 0     
     for base in motif:
-        if base == "A" or base == "T":
-            AT += 1
-        elif base == "C" or base == "G":
-            GC += 1
+        match base:
+            case "A" | "T":
+                AT += 1
+            case "G" | "C":
+                GC += 1
     p = ((0.5 * gc)**GC) * ((0.5 * (1 - gc))**AT)
     return 1 - (1-p)**n
