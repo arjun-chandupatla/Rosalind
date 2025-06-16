076c4ad9b9fda83f20d75f7def579de05ac3c475
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-15 21:36:07 -0500

    Update INOD.py

--- solutions/INOD.py
+++ solutions/INOD.py
@@ -3,3 +3,4 @@
 # In other words, there is one and only one path that connects any two edges in a tree
 
 # An unrooted tree is a tree where every internal node has a degree of 3
+# In contrast, a rooted tree has a root with degree 2, while all other internal nodes have degree 3
