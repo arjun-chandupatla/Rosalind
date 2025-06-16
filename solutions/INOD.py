06df2ead4df410bf099d002a377f462410ecfcd2
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-15 21:36:24 -0500

    Update INOD.py

--- solutions/INOD.py
+++ solutions/INOD.py
@@ -1,6 +1,8 @@
-# Find the number of internal nodes of an unrooted binary tree with n leaves
-# A tree is an undirected graph where all vertices are connected
-# In other words, there is one and only one path that connects any two edges in a tree
+'''
+Find the number of internal nodes of an unrooted binary tree with n leaves
+A tree is an undirected graph where all vertices are connected
+In other words, there is one and only one path that connects any two edges in a tree
 
-# An unrooted tree is a tree where every internal node has a degree of 3
-# In contrast, a rooted tree has a root with degree 2, while all other internal nodes have degree 3
+An unrooted tree is a tree where every internal node has a degree of 3
+In contrast, a rooted tree has a root with degree 2, while all other internal nodes have degree 3
+'''
