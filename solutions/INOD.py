0f5502ac8a55a206497cdd3913cedb4d23e19e27
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-15 21:38:15 -0500

    Update INOD.py

--- solutions/INOD.py
+++ solutions/INOD.py
@@ -4,5 +4,5 @@ A tree is an undirected graph where all vertices are connected and no cycles are
 and a binary tree is a tree where each internal node has degree 3
 In other words, there is one and only one path that connects any two edges in a tree
 
-The number of internal nodes of an unrooted
+The number of internal nodes of an unrooted binary tree is the number of edges minus 2
 '''
