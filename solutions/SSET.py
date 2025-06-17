4ea2824c80050c7ff3a1afc9c847cdcb9515ab44
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-16 23:15:28 -0500

    Update SSET.py

--- solutions/SSET.py
+++ solutions/SSET.py
@@ -1,7 +1,7 @@
 # Given a positive intenger n <= 1000, find the number of subsets of the set {1, 2, ..., n}
 # modulo 1000000
 
-from itertools import combinations
+
 
 def subsets(n: int) -> int:
     num = 1
