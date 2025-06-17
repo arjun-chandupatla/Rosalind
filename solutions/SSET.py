b35b4995865c23021122cef4f26d305cb0295440
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-16 23:17:15 -0500

    Update SSET.py

--- solutions/SSET.py
+++ solutions/SSET.py
@@ -1,6 +1,8 @@
 # Given a positive intenger n <= 1000, find the number of 
 # subsets of the set {1, 2, ..., n} modulo 1000000
 
+# For every item in the set, the item can either be in the subset or not
+# This means that there are 2^n possible subsets for a set of length n
 def subsets(n: int) -> int:
     num = 1
     for i in range(1, n + 1):
