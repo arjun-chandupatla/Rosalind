57f4c84e3c3c9f979cd2c704f4c560e4a08e6a7f
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-23 14:41:30 -0500

    Update KMER.py

--- solutions/KMER.py
+++ solutions/KMER.py
@@ -1,8 +1,8 @@
 # Given a DNA string s, find the 4-mer composition of s
 
 # A kmer is a string of length k, in this case being a DNA string of length k
-# The kmer composition of a string is the number of times each possible kmer appears in it
-# in order alphabetically
+# The kmer composition of a string is the number of times each possible kmer 
+#appears in it in order alphabetically
 
 import itertools
 
