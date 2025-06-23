f61e8145e2f6223a254b0ab9f4b8d73e06bee3d1
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-23 14:22:14 -0500

    Update KMER.py

--- solutions/KMER.py
+++ solutions/KMER.py
@@ -3,3 +3,13 @@
 # A kmer is a string of length k, in this case being a DNA string of length k
 # The kmer composition of a string is the number of times each possible kmer appears in it
 # in order alphabetically
+
+
+
+
+def getKmers():
+    temp = list(itertools.product(["A", "C", "G", "T"], repeat=4))
+    kmers = [""] * len(temp)
+    for i in range(len(temp)):
+        kmers[i] = "".join(temp[i])
+    return kmers
