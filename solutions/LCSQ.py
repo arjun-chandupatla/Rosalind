a5d0bf4231110de5cf977f6a1f81087d07f7abc6
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-06-25 20:57:54 -0500

    Update LCSQ.py

--- solutions/LCSQ.py
+++ solutions/LCSQ.py
@@ -7,20 +7,24 @@ def LCS(s1: str, s2: str):
     n = len(s2)
   
     arr = [[0]  * (n+1) for _ in range(m + 1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
+    for i in range(1, len(sq) + 1):
+        for j in range(1, len(sq) + 1:
             if match(s1, s2, i, j):
                 arr[i][j] = arr[i-1][j-1] + 1
             else:
                 arr[i][j] = max(arr[i-1][j], arr[i][j-1])
     lcs = ""
+
+    p = len(s1)
+    q = len(s2)
+    
     while i > 0 and j > 0:
         if match(s1, s2, i, j):
             lcs += s1[i-1]
-            m -= 1
-            n -= 1
+            p -= 1
+            q -= 1
         elif arr[i-1][j] > arr[i][j-1]:
-            m -= 1
+            p -= 1
         else:
-            n -= 1
+            q -= 1
     return lcs[::-1]
