28f07fad3409a6045866a43f7650c75d2219a56c
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-12-21 18:48:00 -0600

    Update mmch.py

--- solutions/mmch.py
+++ solutions/mmch.py
@@ -1,1 +1,22 @@
 # given an rna strand, find the number of maximum matchings
+
+
+
+def maximumMatchings(rna: str) -> int:
+    a = 0
+    c = 0
+    g = 0
+    u = 0
+    for base in rna:
+        if base == "A":
+            a += 1
+        elif base == "C":
+            c += 1
+        elif base == "G":
+            g += 1
+        elif base == "U":
+            u += 1
+
+    auMatchings = perm(max(a, u), min(a, u))
+    gcMatchings = perm(max(g, c), min(g, c))
+    return auMatchings*gcMatchings
