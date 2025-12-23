c8840a8ab14ae24d744e22d562541327ac63ed69
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-12-23 11:50:12 -0600

    Add reverse complement function for DNA sequences
    Implement reverse complement function for DNA.

--- solutions/corr.py
+++ solutions/corr.py
@@ -1,1 +1,17 @@
 #error correction in reads
+
+
+def rc(dna: str) -> str:
+    s = ""
+    for base in dna:
+        match base:
+            case "A":
+                s += "T"
+            case "C":
+                s += "G"
+            case "G":
+                s += "C"
+            case "T":
+                s += "A"
+    return s[::-1]
+
