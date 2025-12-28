3240c3a759e30ae9f957fb4eea933c6549de7b1e
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-12-28 15:19:00 -0600

    Add probability calculation function for sequences

--- solutions/eval.py
+++ solutions/eval.py
@@ -1,1 +1,11 @@
 
+def prob(n: int, seq: str, gc_arr: list[float]):
+    for gc in gc_arr:
+        p = 1
+        for base in seq:
+            if base == "A" or base == "T":
+                p *= (1-gc)/2
+            elif base == "C" or base == "G":
+                p *= gc/2
+        p *= n-len(seq)+1
+        yield p
