f5b85108b252347ad2905608ce20f5b5e8f0108c
arjun-chandupatla <77982240+arjun-chandupatla@users.noreply.github.com>
2025-12-28 15:19:17 -0600

    Update eval.py

--- solutions/eval.py
+++ solutions/eval.py
@@ -1,3 +1,4 @@
+from math import log10
 
 def prob(n: int, seq: str, gc_arr: list[float]):
     for gc in gc_arr:
@@ -9,3 +10,22 @@ def prob(n: int, seq: str, gc_arr: list[float]):
                 p *= gc/2
         p *= n-len(seq)+1
         yield p
+
+
+i = open("input.txt", "r")
+lines = i.readlines()
+i.close()
+
+n = int(lines[0].strip())
+seq = lines[1].strip()
+arr = list(map(float, lines[2].strip().split()))
+
+ans = ""
+for p in prob(n, seq, arr):
+    ans += (str(p) + " ")
+
+
+h = open("output.txt", "w")
+h.write(ans)
+h.close()
+
